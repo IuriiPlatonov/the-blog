0b7513f68f1c78def551e2c197f0ed9d01e8051d
Платонов Юрий Васильевич <platonov230388@gmail.ru>
2021-08-06 11:51:47 +0300

    feat: stage 5. added method restore password by email

--- src/main/java/org/example/theblog/controllers/ApiAuthController.java
+++ src/main/java/org/example/theblog/controllers/ApiAuthController.java
@@ -1,6 +1,8 @@
 package org.example.theblog.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.example.theblog.service.AuthService;
+import org.example.theblog.service.MailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +10,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class ApiAuthController {
 
     private final AuthService authService;
-
-
-    public ApiAuthController(AuthService authService) {
-        this.authService = authService;
-    }
+    private final MailService mailService;
 
     @GetMapping("/check")
     public AuthService.AuthResponse settings(Principal principal) {
@@ -41,5 +40,10 @@ public class ApiAuthController {
     @GetMapping("/logout")
     public ResponseEntity<AuthService.AuthResponse> logout() {
         return ResponseEntity.ok(authService.logout());
+    }
+
+    @PostMapping("/restore")
+    public ResponseEntity<MailService.MailResponse> restore(@RequestBody MailService.MailRequest request) {
+        return ResponseEntity.ok(mailService.restore(request));
     }
 }
