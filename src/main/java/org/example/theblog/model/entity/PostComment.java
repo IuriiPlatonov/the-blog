585313deb2d07958e45ce475792b22d070ed5f12
Платонов Юрий Васильевич <platonov230388@gmail.ru>
2021-09-10 00:29:34 +0300

    fix: Replacing the @Data annotation in db entities with @Getter @Setter

--- src/main/java/org/example/theblog/model/entity/PostComment.java
+++ src/main/java/org/example/theblog/model/entity/PostComment.java
@@ -1,12 +1,14 @@
 package org.example.theblog.model.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Setter
+@Getter
 @Table(name = "post_comments")
 public class PostComment {
 
